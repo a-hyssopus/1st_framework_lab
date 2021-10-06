dcfe18128b264aec909c4bf0a73fbb4fd2cc00a1
dasha <misery.hatred.anger@gmail.com>
2021-10-06 20:46:22 +0300

    1st laboratory is done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ExponentiationComponent } from './exponentiation/exponentiation.component';
+import {FormsModule} from "@angular/forms";
+import { StudentlistComponent } from './studentlist/studentlist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExponentiationComponent
+    ExponentiationComponent,
+    StudentlistComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
